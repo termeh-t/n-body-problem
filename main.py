b6cdb36a177fb2e2a0153786fe97cf678225143f
Tears <85892741+Ohtears@users.noreply.github.com>
2024-05-26 00:22:16 +0330

    initial values

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+# Constants
+G = 6.67430e-11  
+m1 = 4e30  
+m2 = 2e30
+
+M = m1+m2
+
+rdist = 2e10
+
+x1 = -(m2/M)*rdist
+
+x2 = (m1/M)*rdist
+
+# 
+# star1 = sphere(pos=vector(x1,0,0), radius=4e9)
+
+Rcom = 
+
